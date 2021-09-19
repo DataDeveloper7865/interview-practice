a0dd57e494f7afb129e88689c6717086f5b74170
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-19 15:40:08 -0700

    added component 2

---
+++ src/components/component2/component2.js
@@ -0,0 +1,14 @@
+import react from "react";
+import './component2.css'
+
+function Component2() {
+
+
+    return (
+        <div className='component-2'>
+            Hello world from component1
+        </div>
+    )
+}
+
+export default Component2;
