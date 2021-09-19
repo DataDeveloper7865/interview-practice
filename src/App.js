517218516e4d258124d3d783543f7f05954fa237
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-19 15:37:09 -0700

    added component2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import Component1 from './components/component1';
+import Component1 from './components/component1/component1';
+import Component2 from './components/component2/component2';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <Component1 />
+      <Component2 />
     </div>
   );
 }
