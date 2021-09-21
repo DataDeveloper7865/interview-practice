4fdefc9b380e93cb9bc51844aaaa08988d60fcc3
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-21 16:29:08 -0700

    added list of books

---
+++ src/components/books.js
@@ -0,0 +1,29 @@
+let bookList = [
+    {
+        title: "book1",
+        rating: 1,
+        datePub: '2020-10-10'
+    },
+    {
+        title: "book2",
+        rating: 2,
+        datePub: '2019-02-10'
+    },
+    {
+        title: "book3",
+        rating: 6,
+        datePub: '2018-10-10'
+    },
+    {
+        title: "book4",
+        rating: 5,
+        datePub: '2017-10-10'
+    },
+    {
+        title: "book5",
+        rating: 4,
+        datePub: '2016-10-10'
+    }
+];
+
+export default bookList;
