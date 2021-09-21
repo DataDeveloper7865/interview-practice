1c413b0973b2a4224fe7f4378da74268d9982b02
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-21 16:28:56 -0700

    added ordering functions

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import './App.css';
 import Component1 from './components/component1/component1';
 import Component2 from './components/component2/component2';
 import UseEffectEx from './components/useEffectExample/useEffectEx';
+import Booklist from './components/books';
+import OrderList from './components/orderList.js/OrderList';
+import { useState } from 'react/cjs/react.development';
 
 function App() {
+  const [books, setBooks] = useState(Booklist)
+
+  function orderBooksByRating() {
+    console.log('order books was clicked')
+    let orderBooks = [...books].sort((a,b) => a.rating - b.rating);
+    setBooks(orderBooks);
+  }
+
+  function orderBooksByDate() {
+    console.log('order books by date was clicked');
+    let orderedBooks = [...books].sort((a, b) => new Date(a.datePub) - new Date(b.datePub));
+    setBooks(orderedBooks);
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +29,9 @@ function App() {
       <Component1 />
       <Component2 />
       <UseEffectEx />
+      <button onClick={orderBooksByRating}> Order Da Books By Rating </button>
+      <button onClick={orderBooksByDate}> Order Da Books By Date </button>
+      <OrderList books={books} />
     </div>
   );
 }
